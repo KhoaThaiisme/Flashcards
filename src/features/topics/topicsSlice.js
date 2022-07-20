79d275906672654eed1682b3a0e2a75b612483db
KhoaThaiisme <khoathai@khoas-MBP.lan>
2022-07-20 11:08:56 -0500

    first commit

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const options = {
+    name: 'topics',
+    initialState: {
+        topics: {}
+    },
+    reducers: {
+        addTopic: (state, action) => {
+            const {id, name, icon } = action.payload;
+            state.topics[id] = {
+                id: id,
+                name: name,
+                icon: icon,
+                quizIds: []
+            };
+        },
+        addQuizId: (state, action) => {
+            const { quizId, topicId } = action.payload;
+            state.topics[topicId].quizIds.push(quizId);
+        },
+    },
+    //extra reducers here
+};
+
+export const topicsReducer = createSlice(options);
+//selector
+export const selectTopics = (state) => state.topics.topics;
+// actions + reducers
+export const { addTopic, addQuizId } = topicsReducer.actions;
+export default topicsReducer.reducer;
